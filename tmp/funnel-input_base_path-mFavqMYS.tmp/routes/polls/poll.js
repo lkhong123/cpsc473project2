cb1b3b5e8d91930f08e58727001215847b28c77c
Phong Vu <josphongvu@yahoo.com>
2017-05-11 22:25:16 -0700

    add Timer

---
+++ tmp/funnel-input_base_path-mFavqMYS.tmp/routes/polls/poll.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    actions: {
+        voteForOption(poll, option) {
+            option.incrementProperty('votes');
+            let choiceValue;
+
+            if(option.value === poll.options[0].value) {
+                choiceValue = '1';
+            } else if(option.value === poll.options[1].value) {
+                choiceValue = '2';
+            } else if(option.value === poll.options[2].value){
+                choiceValue = '3';
+            }
+
+            let response = {
+                header: 'response',
+                choice: choiceValue
+            };
+            this.sockjs.sendInfo(JSON.stringify(response));
+        }
+    },
+    model(params) {
+        return this.get('store').getPollById(params.poll_id);
+    },
+
+    store: Ember.inject.service()
+});
